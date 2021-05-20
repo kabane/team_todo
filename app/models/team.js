caa1806b01030572d4d674808bc7f66123df57db
kabane <kabaneshi@gmail.com>
2021-05-21 03:03:35 +0900

    Teamに関する処理を一部メソッド化

--- app/models/team.js
+++ app/models/team.js
@@ -10,6 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      this.Member = models.Member;
+      
       this.Members = this.hasMany(models.Member, {
         foreignKey: 'teamId'
       });
@@ -23,6 +25,33 @@ module.exports = (sequelize, DataTypes) => {
         as: 'owner'
       });
     }
+
+    static async createWithOwner(user, params) {
+      return await sequelize.transaction(async (t) => {
+        const team = this.build(params);
+        team.set({
+          ownerId: user.id
+        });
+        await team.save({ fields: ['name', 'ownerId'], transaction: t });
+        await this.Member.create({
+          teamId: team.id,
+          userId: user.id,
+          role: 1
+        }, { transaction: t });
+
+        return team;
+      });
+    }
+
+    async isManager(user) {
+      return await this.countMembers({
+        where: {
+          role: 1,
+          userId: user.id
+        }
+      }) != 0;
+    }
+
   };
   Team.init({
     name: {
